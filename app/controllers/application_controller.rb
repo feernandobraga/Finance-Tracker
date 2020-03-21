10bd4640e45584d4f73fe40c650ee60905cbcc66
Fernando <fernando.bragga@gmail.com>
2020-03-21 18:56:53 +1100

    Add stock model and iex api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  # this is from the devise gem and it's used so the user can't do anything before logging in.
   before_action :authenticate_user!
 
 end
